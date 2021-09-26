0e83c762abb49000d5eba6545cbc590585ed7f70
Moe Khan <m_a_khan_usa@yahoo.com>
2021-09-26 11:47:12 -0700

    stage_01 util added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,5 +1,14 @@
 
-
 import os
 import yaml
+
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+
+    return content
+
+
+
 
